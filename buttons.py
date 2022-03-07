3a509e43541777ad54f2171bb5d5c3a3006ff1d3
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2022-03-07 14:50:47 +0530

    buttons, resizing and grid system

---
+++ buttons.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root =Tk()
+
+def myClick():
+    myLabel = Label(root, text="Look! I clicked a button!")
+    myLabel.pack()
+
+# myButton = Button(root, text="Click Me", state=DISABLED)
+# myButton = Button(root, text="Click Me", padx=50, pady=50)
+myButton = Button(root, text="Click Me", command=myClick)
+myButton.pack()
+
+root.mainloop()
--- grid.py
+++ grid.py
@@ -4,8 +4,10 @@ root = Tk()
 # creating a label widget and giving it a text
 myLabel1 = Label(root, text="Hello World")
 myLabel2= Label(root, text="My name is Sidra Rihan")
+
 # shoving it onto the screen
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+myLabel2.grid(row=1, column=5)
+
 
 root.mainloop()
