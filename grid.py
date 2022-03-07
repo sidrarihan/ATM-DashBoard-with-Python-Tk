7c4dc2ecdd449914fe10ee8bb5f562ee3b068c15
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2022-03-07 11:59:57 +0530

    grid exp

---
+++ grid.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+# creating a label widget and giving it a text
+myLabel1 = Label(root, text="Hello World")
+myLabel2= Label(root, text="My name is Sidra Rihan")
+# shoving it onto the screen
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
+root.mainloop()
---
+++ helloworldtk.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+# creating a label widget and giving it a text
+myLabel = Label(root, text="Hello World")
+# shoving it onto the screen
+myLabel.pack()
+
+root.mainloop()
